98dc87e7e361835d5edc5da03523c99800c2d793
theyousry <theyousry@gmail.com>
2018-12-10 02:59:45 +0200

    Some tests Added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,51 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('Every feed element has a "URL" defined', function() {
+             for (let i=0; i < allFeeds.length; i++) {
+                 expect(allFeeds[i]['url']).toBeDefined();
+                 expect(allFeeds[0]['url'].length).toBeGreaterThan(0);
+               }
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('Every feed element has a "name" defined', function() {
+            for (let i=0; i < allFeeds.length; i++) {
+              expect(allFeeds[i]['name']).toBeDefined();
+              expect(allFeeds[0]['name'].length).toBeGreaterThan(0);
+            }
+      });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         it('the menu element is hidden by default', function() {
+           expect($("body")[0]).toHaveClass('menu-hidden');
+         });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('the menu changes visibility when the menu icon is clicked', function() {
+            var body = $("body")[0];
+
+            document.querySelector('.icon-list').click();
+            expect(body.className).toBe('');
+
+             document.querySelector('.icon-list').click();
+             expect(body. className).toBe('menu-hidden');
+          });
+     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
