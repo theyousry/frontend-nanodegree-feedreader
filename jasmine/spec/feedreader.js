e5e1fdc30f303d379c40823436b0c2846c951d41
theyousry <theyousry@gmail.com>
2018-12-10 03:19:00 +0200

    Rest of tests Added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,18 +78,40 @@ $(function() {
      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+         beforeEach(function(done) {
+             loadFeed(0, function() {
+                 done();
+             });
+         });
+          it('the loadFeed func. called and completes its work', function(done) {
+             expect($('.feed')[0].getElementsByClassName('entry').length).toBeGreaterThan(0);
+             done();
+         });
+     });
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+          beforeEach(function(done) {
+              loadFeed(1, function() {
+                  feedInnerText = $('.feed')[0].innerText;
+                  loadFeed(2, function() {
+                      done();
+                  });
+              });
+          });
+         it('a new feed loaded by loadFeed func. that content actually changes', function(done) {
+            expect($('.feed')[0].innerText === feedInnerText).not.toBe(true);
+            done();
+        });
+    });
 }());
